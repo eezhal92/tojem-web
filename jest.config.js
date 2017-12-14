f150e8cd3dac5fdd3abf7bb5842fc0554d89aee4
eezhal92 <arkinthesky.69@gmail.com>
2017-12-14 13:38:47 +0800

    setup vue test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
 module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue',
+  ],
   moduleNameMapper: {
     '^app/(.*)$': '<rootDir>/app/$1',
+  },
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
   },
 };
--- resource/js/components/ProductItem.vue
+++ resource/js/components/ProductItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="product-item">
-    <div>{{ product.name }}</div>
+    <div class="product-item__name">{{ product.name }}</div>
     <div>{{ product.price | rupiah }}</div>
     <div>
       <button @click="addToCart" :id="'add-product-item-' + product.id" class="bg-white text-xs hover:bg-blue-dark text-blue border border-blue font-bold py-2 px-4 rounded-full">Tambah ke keranjang</button>
