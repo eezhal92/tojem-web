ba9271b9fd54c2a3c2d7a2bee319c9df64520c40
eezhal92 <arkinthesky.69@gmail.com>
2018-05-05 12:58:12 +0800

    base landing

--- app/routes/tojem/index.js
+++ app/routes/tojem/index.js
@@ -8,11 +8,15 @@ import homeController from 'app/controllers/home-controller';
 
 const router = express.Router();
 
-router.get('/', homeController.showHomePage);
+router.get('/', (request, response) => {
+  response.render('landing-page/grow-together');
+});
 router.get('/join', (request, response) => {
   response.render('landing-page/grow-together');
 });
-router.get('/cari', homeController.showSearchPage);
-router.get('/item/:id', homeController.showProductDetailPage);
+
+// Disabled, not ready yet
+// router.get('/cari', homeController.showSearchPage);
+// router.get('/item/:id', homeController.showProductDetailPage);
 
 export default router;
