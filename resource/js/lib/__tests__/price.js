1e65a43cb0b8e49a600b95ec2945e905db58aed8
Guntur <poetra.guntur@gmail.com>
2018-05-06 19:35:54 +0700

    make eslint happy.

--- resource/js/lib/__tests__/price.js
+++ resource/js/lib/__tests__/price.js
@@ -1,9 +1,9 @@
-import price from '../price';
+import { filterNominal } from '../price';
 
 describe('price', () => {
   test('it should be get only the number of price', () => {
-    expect(price.filterNominal('Rp. 50,000')).toBe(50000);
-    expect(price.filterNominal('Rp. 45.000')).toBe(45000);
-    expect(price.filterNominal('100.000')).toBe(100000);
+    expect(filterNominal('Rp. 50,000')).toBe(50000);
+    expect(filterNominal('Rp. 45.000')).toBe(45000);
+    expect(filterNominal('100.000')).toBe(100000);
   });
 });
