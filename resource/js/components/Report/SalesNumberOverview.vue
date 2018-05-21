02d5e7df2ca46b2120769cfb968230b6e7fc79bc
eezhal92 <arkinthesky.69@gmail.com>
2018-05-21 21:52:26 +0800

    change stats wording

--- resource/js/components/Report/SalesNumberOverview.vue
+++ resource/js/components/Report/SalesNumberOverview.vue
@@ -2,7 +2,7 @@
   <div class="sales-number-overview">
     <div class="flex">
       <div class="xl:w-1/4 lg:w-1/2 md:w-1/2 sm:w-full m-4 p-3 overflow-hidden hover:shadow-lg">
-        <p>Order</p>
+        <p>Transaksi</p>
         <p class="text-3xl">{{ transactionNumber }}</p>
       </div>
       <div class="text-black xl:w-1/4 lg:w-1/2 md:w-1/2 sm:w-full m-4 p-3 overflow-hidden hover:shadow-lg">
